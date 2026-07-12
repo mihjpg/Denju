c7d3db07cfb852e9653123f32ee963cf411c0590
mihjpg <oiayumi.tk@gmail.com>
2026-07-12 14:00:53 -0300

    feat: BCrypt

--- src/main/java/com/denju/denju/service/UserService.java
+++ src/main/java/com/denju/denju/service/UserService.java
@@ -4,6 +4,7 @@ import com.denju.denju.dto.UserRequestDTO;
 import com.denju.denju.dto.UserResponseDTO;
 import lombok.RequiredArgsConstructor;
 import com.denju.denju.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.denju.denju.entities.Usuario;
 
@@ -12,11 +13,12 @@ import com.denju.denju.entities.Usuario;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserResponseDTO criarUsuario(UserRequestDTO request) {
         Usuario usuario = Usuario.builder()
                 .nome(request.nome())
-                .senha(request.senha())
+                .senha(passwordEncoder.encode(request.senha()))
                 .build();
 
         Usuario salvo = userRepository.save(usuario);
